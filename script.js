8636ee045aac5ad90de99e0b722f9f018b472939
vansarath11 <148926519+vansarath11@users.noreply.github.com>
2023-10-25 08:48:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Typing animation------------------------------------------------
+var typed = new Typed(".typing", {
+    strings:["","Facebook Page", "Web Developer", "Designer", "YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
